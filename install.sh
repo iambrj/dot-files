63619e2c176d25bc0326dd42aafc27df1c037751
iambrj <joshibharathiramana@gmail.com>
2019-11-30 06:19:02 +0530

    Change hardcoded ~ to /home/hogfather

--- install.sh
+++ install.sh
@@ -1,15 +1,15 @@
 #!/bin/sh
 
 # Set up i3wm
-cp ./config ~/.config/i3/
-cp ./i3blocks.conf ~/.config/i3/
+cp ./config $HOME/.config/i3/
+cp ./i3blocks.conf $HOME/.config/i3/
 sudo cp ./i3blocks-scripts/* /usr/share/i3blocks
 
 # Set up vim
-cp ./.vimrc ~
+cp ./.vimrc $HOME
 
 # Set up zsh
-cp ./.zshrc ~
+cp ./.zshrc $HOME
 
 # Set up Xdefaults
-cp ./.Xdefaults ~
+cp ./.Xdefaults $HOME
