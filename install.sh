25f80c114005b958f722bcbcf5c808ca9ee7ca19
iambrj <joshibharathiramana@gmail.com>
2020-02-14 00:52:23 +0530

    Update colors

--- install.sh
+++ install.sh
@@ -1,15 +1,33 @@
 #!/bin/sh
 
+configDir="$HOME/.config/"
+rofiDir="${configDir}rofi/"
+i3Dir="${configDir}i3/"
+i3BlockScriptsDir="/usr/share/i3blocks"
+
+i3DirFiles="./i3blocks.conf ./config"
+i3BlockScripts="./i3blocks-scripts/*"
+homeFiles="${HOME}/.zshrc ${HOME}/.Xdefaults"
+
+# Create directories, if they don't already exist
+if [ -d "$configDir" ]; then
+	echo "Installing config files in ${configDir}..."
+else
+	mkdir "$configDir"
+fi
+# TODO: create missing directories
+
 # Set up i3wm
-cp ./config $HOME/.config/i3/
-cp ./i3blocks.conf $HOME/.config/i3/
+cp "$i3DirFiles" "$i3Dir"
 sudo cp ./i3blocks-scripts/* /usr/share/i3blocks
+sudo cp "$i3BlockScripts" "$i3BlockScriptsDir"
 
 # Set up vim
-cp ./.vimrc $HOME
+cp ./.vimrc "$HOME"
+# TODO set up YCM
 
-# Set up zsh
-cp ./.zshrc $HOME
+# Set up homefiles
+cp "$homeFiles" "$HOME"
 
-# Set up Xdefaults
-cp ./.Xdefaults $HOME
+# rofi theme
+cp 
