89491193b18f3e0185834e14c5a9d2bc9e97d93e
iambrj <joshibharathiramana@gmail.com>
2021-10-31 18:55:57 +0530

    Update skeletons

---
+++ skeleton.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ull unsigned long long
+#define ll long long
+#define MODK 1000000007
+
+void solve() {
+  
+}
+
+int main() {
+  std::ios_base::sync_with_stdio(false);
+  cin.tie(nullptr);
+  //int t; cin >> t; while(t--)
+  solve();
+}
---
+++ skeleton.md
@@ -0,0 +1,8 @@
+---
+fontfamily: mathpazo
+title: ''
+author: 'Bharathi Ramana Joshi, 2019121006'
+date: 'dd-mm-2021'
+header-includes:
+  - \hypersetup{colorlinks=true, urlcolor=blue}
+---
